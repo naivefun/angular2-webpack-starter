9058f2cd95fe3170b3d83287278cd2d09c9caf54
PeiSong Xiong <peisongo@gmail.com>
2017-03-16 17:03:27 +0800

    added text editor modes

--- src/app/@components/text-editor/text-editor.component.ts
+++ src/app/@components/text-editor/text-editor.component.ts
@@ -1,4 +1,6 @@
-import { AfterViewInit, Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { TextEditorMode } from './text-editor.model';
+import * as _ from 'lodash';
 
 declare const ace: any;
 
@@ -13,16 +15,59 @@ declare const ace: any;
     }
   `]
 })
-export class TextEditorComponent implements AfterViewInit {
+export class TextEditorComponent implements AfterViewInit, OnChanges {
 
   @Input() public aceId;
+  @Input() public mode = TextEditorMode.YAML; // defaults to YAML
+  @Input() public text: string;
 
   private editor: any;
+  private session: any;
 
-  public  ngAfterViewInit(): void {
+  public ngAfterViewInit(): void {
     this.editor = ace.edit(this.aceId);
     this.editor.setTheme('ace/theme/github');
-    this.editor.getSession().setMode('ace/mode/javascript');
+    this.session = this.editor.getSession();
+    this.setMode(this.mode);
+
+    if (this.text) {
+      this.setText(this.text);
+    }
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    let mode = changes['mode'];
+    if (mode) {
+      this.setMode(this.mode);
+    }
+
+    let text = changes['text'];
+    if (text) {
+      this.setText(this.text);
+    }
+  }
+
+  public getValue(): string {
+    if (this.editor) {
+      return this.editor.getValue();
+    }
+  }
+
+  public setValue(text: string) {
+    if (this.editor) {
+      this.editor.setValue(text || '', 1);
+    }
+  }
+
+  private setMode(mode: TextEditorMode) {
+    mode = mode || TextEditorMode.YAML;
+    this.editor.getSession().setMode(`ace/mode/${_.toLower(mode + '')}`);
+  }
+
+  private setText(text: string) {
+    let currentValue = this.getValue();
+    // TODO: compare text with current value
+    this.setValue(text);
   }
 
 }
---
+++ src/app/@components/text-editor/text-editor.model.ts
@@ -0,0 +1,6 @@
+export enum TextEditorMode {
+  JSOBJECT = <any> 'Javascript Object',
+  HJSON = <any> 'HJSON',
+  JSON = <any> 'JSON',
+  YAML = <any> 'YAML'
+}
